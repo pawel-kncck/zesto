9404ec2f4cc250c46c60c8d4d298d3459d497aa1
Pawel <pawel.kuncicki@gmail.com>
2020-11-26 11:44:48 +0100

    load standard view as default

--- src/containers/Workspace.js
+++ src/containers/Workspace.js
@@ -6,6 +6,7 @@ import { makeStyles, Paper } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { loadQuizState } from '../store/quiz.actions';
 import { loadAnswersState } from '../store/answers.actions';
+import { setEditMode } from '../store/editMode.actions';
 import firebase from '../firebase';
 import LoadingScreen from '../componenets/LoadingScreen';
 import { objectToJson } from '../utils/converters';
@@ -46,6 +47,10 @@ function Workspace({ loadState, ...props }) {
     };
   }, [loadState, quizId]);
 
+  useEffect(() => {
+    props.loadStandardView();
+  }, []);
+
   return (
     <>
       <LowerNavbar upToDate={isDatabaseSyncWithState} quizId={quizId} />
@@ -74,6 +79,9 @@ const mapDispatchToProps = (dispatch) => {
     loadState: (body, answers) => {
       dispatch(loadQuizState(body));
       dispatch(loadAnswersState(answers));
+    },
+    loadStandardView: () => {
+      dispatch(setEditMode(false));
     },
   };
 };
--- src/store/editMode.reducer.js
+++ src/store/editMode.reducer.js
@@ -1,51 +1,51 @@
 import * as actionTypes from './editMode.actions';
 
 const initialState = {
-    active: true,
-    caretPosition: {
-        scIndex: null,
-        exIndex: null,
-        pgIndex: null,
-        elIndex: null,
-        caretIndex: null,
-        containerPositionY: null,
-        active: false,
-        modeChangeQueued: false,
-        insertButtonHover: false,
-    }
-}
+  active: false,
+  caretPosition: {
+    scIndex: null,
+    exIndex: null,
+    pgIndex: null,
+    elIndex: null,
+    caretIndex: null,
+    containerPositionY: null,
+    active: false,
+    modeChangeQueued: false,
+    insertButtonHover: false,
+  },
+};
 
 const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case actionTypes.SET_CARET_POSITION:
-			return {
-                ...state,
-                caretPosition: {
-                    ...action.payload,
-                    active: true
-                }
-            }
-        case actionTypes.SET_PARAGRAPH_EDIT_MODE:
-            return {
-                ...state,
-                caretPosition: {
-                    ...state.caretPosition,
-                    ...action.payload,
-                },
-            }
-        case actionTypes.SET_EDIT_MODE:
-            return {
-                ...state,
-                active: action.payload
-            }
-        case actionTypes.INSERT_BUTTON_HOVER_STATE:
-            return {
-                ...state,
-                insertButtonHover: action.payload.is_active
-            }
-		default:
-			return state;
-	}
-}
+  switch (action.type) {
+    case actionTypes.SET_CARET_POSITION:
+      return {
+        ...state,
+        caretPosition: {
+          ...action.payload,
+          active: true,
+        },
+      };
+    case actionTypes.SET_PARAGRAPH_EDIT_MODE:
+      return {
+        ...state,
+        caretPosition: {
+          ...state.caretPosition,
+          ...action.payload,
+        },
+      };
+    case actionTypes.SET_EDIT_MODE:
+      return {
+        ...state,
+        active: action.payload,
+      };
+    case actionTypes.INSERT_BUTTON_HOVER_STATE:
+      return {
+        ...state,
+        insertButtonHover: action.payload.is_active,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
